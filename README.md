e62315ebc78e4750e0397886eb6543994e31234b
Robyn <robyn.g@hotmail.fr>
2022-07-28 23:06:09 -0400

    Update README + json data

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # DofusScraper
-Scraper pour l'encyclopédie de Dofus
+Scraper en typescript pour l'encyclopédie de Dofus
 
-Contient des données JSON statiques et générées par le scraper.
+Peut s'utiliser comme submodule pour exploiter les données déjà générées.
+
+Contient la base de données JSON générée dans `./data/scraped/`
+
+Contient quelques données JSON statiques dans `./data/static/`
+
+---
+
+Mise en place: `npm install`
+
+Exécuter: 
+1) modifier `scraper.ts` pour appeler les scrapers voulus 
+2) commande `npm run scraper`
--- src/scraper.ts
+++ src/scraper.ts
@@ -1,15 +1,14 @@
-// import axios from 'axios';
 import cheerio from 'cheerio';
 import puppeteer from 'puppeteer'
 import puppeteerextra from 'puppeteer-extra'
 import fs from 'fs';
-
+// static
 import jsonClasses from '../data/static/classes.json';
 import jsonSpellLevels from '../data/static/spellLevels.json'
-
+// scraped
 import jsonSpells from '../data/scraped/spells.json';
-import jsonPetsIds from '../data/scraped/petIds1.json'
-
+import jsonPetsIds from '../data/scraped/petIds.json'
+// scrapers
 import { SpellScraper } from './scrapers/spells';
 import { ItemScraper } from './scrapers/items';
 import { Pets } from './scrapers/pets';
@@ -17,6 +16,10 @@ import { Pets } from './scrapers/pets';
 const dofusurl = "https://www.dofus.com/fr/mmorpg/encyclopedie/";
 const staticurl = "https://static.ankama.com/dofus/www/game/"; // + "spells/55/3772.png"
 
+
+/*
+ *	Décomenter ou ajouter la ligne voulue et rouler le fichier avec `npm run scraper` 
+ */
 
 // SpellScraper.scrapSpellIds();
 // SpellScraper.scrapSpellData();
